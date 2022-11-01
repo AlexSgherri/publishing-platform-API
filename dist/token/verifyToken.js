e4f8fcff5beb84423f8a3960157b5a321d0dc856
Alessandro <a.sgherri92@gmail.com>
2022-11-01 15:22:27 +0100

    fixed verifyToken extension

---
+++ dist/token/verifyToken.js
@@ -0,0 +1,43 @@
+"use strict";
+const jwt = require("jsonwebtoken");
+//THE TOKEN WILL BE SAVE IN HEADERS AS --- "token" ---
+//TOKEN WITH NO OTHERS AUTHORIZATION
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers.token;
+    if (authHeader) {
+        const token = authHeader.split(" ")[1];
+        jwt.verify(token, process.env.JWT_KEY, (err, user) => {
+            if (err)
+                res.status(403).json("Token is not valid!");
+            req.user = user;
+            next();
+        });
+    }
+    else {
+        return res.status(401).json("You are not authenticated");
+    }
+};
+//TOKEN WITH STANDARD LOGIN AUTHORIZATION
+const verifyTokenAndAuthorization = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if (req.user.id === req.params.id || req.user.role == "ADMIN") {
+            next();
+        }
+        else {
+            res.status(403).json("You are not alowed to do that!");
+        }
+    });
+};
+//TOKEN WITH ADMIN LOGIN AUTHORIZATION
+const verifyTokenAndAdmin = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if (req.user.isAdmin == "ADMIN") {
+            next();
+        }
+        else {
+            res.status(403).json("You are not alowed to do that!");
+        }
+    });
+};
+module.exports = { verifyTokenAndAdmin, verifyToken, verifyTokenAndAuthorization };
+//# sourceMappingURL=verifyToken.js.map
