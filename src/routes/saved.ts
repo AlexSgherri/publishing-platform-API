ca00e0e298de8abbdf9ff19c527b854cab602edf
Alessandro <a.sgherri92@gmail.com>
2022-11-08 20:23:42 +0100

    feat: add get saved post for user id

--- src/routes/saved.ts
+++ src/routes/saved.ts
@@ -18,20 +18,54 @@ router.post("/", verifyToken, async (req: any, res: any) => {
       save = await prisma.saved.deleteMany({
         where: {
           postId: req.body.postId,
-          userId: req.body.userId
-        }
-      })
+          userId: req.body.userId,
+        },
+      });
     } else {
-       save = await prisma.saved.create({
+      save = await prisma.saved.create({
         data: {
           userId: req.body.userId,
           postId: req.body.postId,
-          saved: true
+          saved: true,
         },
       });
     }
 
     res.status(200).json(save);
+  } catch (err) {
+    res.status(404).json(err);
+  } finally {
+    prisma.$disconnect();
+  }
+});
+
+type SavedPostIdType = {
+  postId: string;
+};
+
+//GET ALL SAVED POST FOR A SINGLE USER BY ID
+router.get("/:id", verifyToken, async (req: any, res: any) => {
+  try {
+    const postsId = await prisma.saved.findMany({
+      where: {
+        userId: req.params.id,
+      },
+      select: {
+        postId: true,
+      },
+    });
+
+    const postIdList = postsId.map((ele: SavedPostIdType) => ele.postId);
+
+    const postsList = await prisma.post.findMany({
+      where: {
+        id: {
+          in: postIdList,
+        },
+      },
+    });
+
+    res.status(200).json(postsList);
   } catch (err) {
     res.status(404).json(err);
   } finally {
