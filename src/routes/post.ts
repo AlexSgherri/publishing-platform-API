a06f4da549c821df7d203b0c5a248382b485f3a3
Alessandro <a.sgherri92@gmail.com>
2022-11-09 12:38:52 +0100

    fix author details retrieved from post GET

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -2,7 +2,6 @@ const router = require("express").Router();
 import { prisma } from "../index";
 const { verifyToken } = require("../token/verifyToken");
 
-
 //FIND ALL POST FOR USERS TOPICS
 router.post("/find", async (req: any, res: any) => {
   try {
@@ -26,13 +25,13 @@ router.get("/find/author/:id", async (req: any, res: any) => {
       where: {
         authorId: { in: req.params.id },
       },
-      include:{
-        author: true
+      include: {
+        author: true,
       },
     });
     res.status(200).json(posts);
   } catch (err) {
-    console.log(err)
+    console.log(err);
     res.status(404).json(err);
   } finally {
     prisma.$disconnect();
@@ -40,7 +39,7 @@ router.get("/find/author/:id", async (req: any, res: any) => {
 });
 
 //CREATE POST
-router.post("/create", verifyToken ,async (req: any, res: any) => {
+router.post("/create", verifyToken, async (req: any, res: any) => {
   try {
     const post = await prisma.post.create({
       data: {
@@ -56,7 +55,7 @@ router.post("/create", verifyToken ,async (req: any, res: any) => {
 
     res.status(200).json(post);
   } catch (err) {
-    console.log("problem")
+    console.log("problem");
     console.error("error executing query:", err);
   } finally {
     prisma.$disconnect();
@@ -64,7 +63,7 @@ router.post("/create", verifyToken ,async (req: any, res: any) => {
 });
 
 //FIND POST BY TOPIC
-router.get("/find/:topic", verifyToken ,async (req: any, res: any) => {
+router.get("/find/:topic", verifyToken, async (req: any, res: any) => {
   try {
     const posts = await prisma.post.findMany({
       where: {
@@ -72,16 +71,17 @@ router.get("/find/:topic", verifyToken ,async (req: any, res: any) => {
           has: req.params.topic,
         },
       },
-      include:{
-        author: true
+      include: {
+        author: {
+          select: { avatar: true,
+          username: true, },
+        },
       },
       take: 10,
     });
 
-    
     res.status(200).json(posts);
   } catch (err) {
-    
     res.status(404).json(err);
   } finally {
     prisma.$disconnect();
